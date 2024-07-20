475fa2ec130989ab457939fda5e2162098ef44b2
Simone Arena <122620587+WasamiKirua@users.noreply.github.com>
2024-07-20 11:26:16 +0200

    Create deps.sh

---
+++ deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pip install --upgrade --force-reinstall --no-cache-dir torch==2.2.0 triton --index-url https://download.pytorch.org/whl/cu121
+pip install "unsloth[cu121-ampere-torch220] @ git+https://github.com/unslothai/unsloth.git"
+pip install wandb colorama
